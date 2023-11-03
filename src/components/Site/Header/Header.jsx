3bb5e5fbca8542c27f1e90f3c7b0e7292d84a6ca
Muslim050 <05-hitman@mail.ru>
2023-11-03 22:07:43 +0500

    bf

--- src/components/Site/Header/Header.jsx
+++ src/components/Site/Header/Header.jsx
@@ -3,10 +3,11 @@ import { motion } from "framer-motion";
 import styles from "src/components/Site/Header/header.module.scss";
 import { Link as ScrollLink } from "react-scroll";
 import Logo from "src/assets/Site/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   const [isActive, setIsActive] = React.useState(false);
+  const location = useLocation();
 
   const toggleActiveClass = () => {
     setIsActive(!isActive);
@@ -15,7 +16,20 @@ const Header = () => {
   const removeActive = () => {
     setIsActive(false);
   };
+  const isHomePage = location.pathname === "/"; // Проверка, является ли текущая страница главной
 
+  const scrollToSection = (sectionId) => {
+    if (isHomePage) {
+      // Если мы на главной странице, прокрутить к соответствующему разделу
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
+  const handleReload = () => {
+    window.location.reload();
+  };
   return (
     <motion.div
       initial={{ opacity: 0, y: -180 }}
@@ -30,45 +44,47 @@ const Header = () => {
     >
       <header className={styles.App_header}>
         <nav className={`${styles.navbar}`}>
-          <Link to={"/"} className={`${styles.logo}`}>
-            <img style={{ height: "50px" }} src={Logo} alt="" />
-          </Link>
+          {isHomePage ? (
+            <Link className={styles.logo} onClick={handleReload}>
+              <img style={{ height: "50px" }} src={Logo} alt="" />
+            </Link>
+          ) : (
+            <Link to={"/"} className={`${styles.logo}`}>
+              <img style={{ height: "50px" }} src={Logo} alt="" />
+            </Link>
+          )}
 
           <ul className={`${styles.navMenu} ${isActive ? styles.active : ""}`}>
-            <li onClick={removeActive}>
-              <ScrollLink
-                to="Технология"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`${styles.navLink}`}
-              >
-                Технология
-              </ScrollLink>
-            </li>
-            <li onClick={removeActive}>
-              <ScrollLink
-                to="форматы"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`${styles.navLink}`}
-              >
-                Рекламные форматы
-              </ScrollLink>
-            </li>
-            <li onClick={removeActive}>
-              <ScrollLink
-                to="Бизнес"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`${styles.navLink}`}
-              >
-                Бизнес модель
-              </ScrollLink>
-            </li>
-            <Link onClick={removeActive} to={"/news"}>
+            {isHomePage && ( // Проверка, что мы находимся на главной странице
+              <>
+                <li onClick={() => scrollToSection("Технология")}>
+                  <span className={`${styles.navLink}`}>Технология</span>
+                </li>
+                <li onClick={() => scrollToSection("форматы")}>
+                  <span className={`${styles.navLink}`}>Рекламные форматы</span>
+                </li>
+                <li onClick={() => scrollToSection("Бизнес")}>
+                  <span className={`${styles.navLink}`}>Бизнес модель</span>
+                </li>
+              </>
+            )}
+
+            {!isHomePage && ( // Проверка, что мы не на главной странице (показать ссылку на главную)
+              <>
+                <Link to={"/"} onClick={removeActive}>
+                  <li className={`${styles.navLink}`}>Технология</li>
+                </Link>
+
+                <Link to={"/"} onClick={removeActive}>
+                  <li className={`${styles.navLink}`}>Рекламные форматы</li>
+                </Link>
+                <Link to={"/"} onClick={removeActive}>
+                  <li className={`${styles.navLink}`}>Бизнес модель</li>
+                </Link>
+              </>
+            )}
+
+            <Link to={"/news"}>
               <li className={`${styles.navLink}`}>Новости</li>
             </Link>
             <Link to={"/login"}>
